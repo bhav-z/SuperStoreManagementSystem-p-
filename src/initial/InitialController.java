ea2c1aa0405c69daf9e40bb4018ffa4aa0cad6e0
bhav-z <bhavika17036@iiitd.ac.in>
2018-12-02 01:20:29 +0530

    linking initial and login

--- src/initial/InitialController.java
+++ src/initial/InitialController.java
@@ -1,6 +1,45 @@
 package initial;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import login.LoginController;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
 public class InitialController {
+
+    @FXML private Button SuperUser;
+    @FXML private Button WarehouseAdmin;
+    @FXML private Button StoreAdmin;
+    @FXML private Button Guest;
+
+    public void buttonClickedForLogin(ActionEvent actionEvent) throws IOException {
+            FXMLLoader loader=new FXMLLoader();
+            loader.setLocation(getClass().getResource("/login/sign_in.fxml"));
+            Parent login_page = loader.load();
+            Scene login_scene = new Scene(login_page);
+            LoginController login=loader.getController();
+            login.setTypeOfUser(((Button)actionEvent.getSource()).getText());
+
+            //This line gets the Stage information
+            Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+
+            window.setScene(login_scene);
+            window.show();
+
+    }
+
+    public void guestButtonClicked(ActionEvent actionEvent){
+
+    }
 
     
 }
