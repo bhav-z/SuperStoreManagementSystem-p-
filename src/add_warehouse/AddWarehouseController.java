c7af7b9c36ed405f6c1e4a50cbf0d917a7567e21
Sudhanshu Mohan <sk22.3.1999@live.in>
2018-12-02 13:54:41 +0530

    addwarehouse

--- src/add_warehouse/AddWarehouseController.java
+++ src/add_warehouse/AddWarehouseController.java
@@ -12,6 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+@SuppressWarnings("ALL")
 public class AddWarehouseController {
     @FXML
     private Button add;
@@ -25,29 +26,60 @@ public class AddWarehouseController {
         try {
             Statement statement=connection.createStatement();
             String warehousename1=name.getText().trim();
+            String sql0="INSERT INTO warehouse_list (name) VALUES ('"+warehousename1+"');";
+            statement.executeUpdate(sql0);
             //https://stackoverflow.com/questions/6260688/how-do-i-use-cascade-delete-with-sql-server
-            String sql1 = "CREATE TABLE "+ warehousename1 +"_categories ( category_id INT NOT NULL , category_name VARCHAR(100) PRIMARY KEY );";
+            String sql1 =
+                    "CREATE TABLE "+ warehousename1 +"_categories " +
+                            "(category_id INT NOT NULL , " +
+                            " category_name VARCHAR(100) PRIMARY KEY );";
             String sql2 =
                     "CREATE TABLE "+ warehousename1 +"_subcategories ( " +
-                            "subcategory_id INT NOT NULL ," +
-                            " category_name VARCHAR(100) PRIMARY KEY , "+
-                            " subcategory_name VARCHAR(100) NOT NULL , "+
-                            " CONSTRAINT fk_inv_cateory_id " +
+                            " subcategory_id INT NOT NULL ," +
+                            " category_name VARCHAR(100) NOT NULL , "+
+                            " subcategory_name VARCHAR(100) PRIMARY KEY , "+
+                            " CONSTRAINT fk_inv_"+warehousename1+"_category_id " +
                             "FOREIGN KEY (category_name)" +
                             "REFERENCES "+ warehousename1 +"_categories (category_name)" +
                             "ON DELETE CASCADE);";
+//            String sql3 =
+//                    "CREATE TABLE "+ warehousename1 +"_itemss ( " +
+//                            " item_id INT NOT NULL ," +
+//                            " subcategory_name VARCHAR(100) NOT NULL , "+
+//                            " item_name VARCHAR(100) NOT NULL , "+
+//                            " CONSTRAINT fk_inv_subcategory_id " +
+//                            "FOREIGN KEY (subcategory_name)" +
+//                            "REFERENCES "+ warehousename1 +"_subcategories (subcategory_name)" +
+//                            "ON DELETE CASCADE);";
+
+            statement.addBatch(sql1);
+            statement.addBatch(sql2);
+//            statement.addBatch(sql3);
+            statement.executeBatch();
+
+            this.createProductsTable();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createProductsTable(){
+        ConnectionU connectionClass = new ConnectionU();
+        Connection connection=connectionClass.getConnection();
+        try {
+            Statement statement=connection.createStatement();
+            String warehousename1=name.getText().trim();
+
             String sql3 =
-                    "CREATE TABLE "+ warehousename1 +"_items ( " +
-                            "item_id INT NOT NULL ," +
-                            " subcategory_name VARCHAR(100) PRIMARY KEY , "+
+                    "CREATE TABLE "+ warehousename1 +"_itemss ( " +
+                            " item_id INT NOT NULL ," +
+                            " subcategory_name VARCHAR(100) NOT NULL , "+
                             " item_name VARCHAR(100) NOT NULL , "+
-                            " CONSTRAINT fk_inv_subcategory_id " +
+                            " CONSTRAINT fk_inv_"+warehousename1+"_subcategory_id " +
                             "FOREIGN KEY (subcategory_name)" +
                             "REFERENCES "+ warehousename1 +"_subcategories (subcategory_name)" +
                             "ON DELETE CASCADE);";
 
-            statement.addBatch(sql1);
-            statement.addBatch(sql2);
             statement.addBatch(sql3);
             statement.executeBatch();
 
